34783bbfbeb4288efd3e86d87c3d6ca862c0e2a4
Sarfo Attah Senior <41247057+SarfoX@users.noreply.github.com>
2021-08-13 03:39:52 +0000

    Add files via upload

---
+++ MatIDA_paramfile.tcl
@@ -0,0 +1,4 @@
+set nbayx 3 
+set nstory 4 
+set sheight "[expr 3.3*$m]"
+set baylength "[expr 6.0*$m]"
---
+++ MatIDA_thfile.tcl
@@ -0,0 +1,7 @@
+set GMdir ""
+set GMfileX "C:/Users/KNUST/Desktop/JackOpenSees/bundle_runIDA/osees/records/RSN97_B.AT2"
+set GMfileY ""
+set GMfileZ ""
+set GMfactorX 49.705
+set GMfactorY 0
+set GMfactorZ 0
