f559c4f3ef18191cfecf6c0ac32b6c0b76123cbc
Sarfo Attah Senior <41247057+SarfoX@users.noreply.github.com>
2021-08-13 03:54:08 +0000

    Add files via upload

---
+++ Incremental Dynamic Analysis (IDA) of 2D frame/2D 2Storey Model with 10Mpa Concrete Strength/standard_paramfile.tcl
@@ -0,0 +1,14 @@
+# Frame geometry
+set nbayx 4
+
+set nstory 3
+
+#set sheight "[list 3.0 3.0 2.725]"
+
+set sheight [list 3.0 3.0 2.725]
+
+#set sheight [list 4.8 3.2 3.2 3.2]
+
+set baylength [list 2.85 3.75 3.75 5.525]
+
+#set baylength [expr 5*$m ]
